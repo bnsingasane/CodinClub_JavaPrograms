626b1f590191c040da3382932c7a112ba8848c13
bhagyashree <bnsingasane@gmail.com>
2020-07-17 01:45:26 +0530

    Adding part time of employee wage

--- employeeWage.java
+++ employeeWage.java
@@ -4,12 +4,15 @@ echo "Welcome to Employee Wage Computation Problem"
 public class employeeWage {
 
 	public static void main(String[] args) {
+		int PART_TIME=1;
 		int IS_FULL_TIME=1;
 		int EMP_WAGE_PER_HR=20;
 		int empHrs=0;
 		int empWage=0;
-		double empCheck=Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
+		double empCheck=Math.floor(Math.random() * 10) % 3;
+		if(empCheck==PART_TIME)
+			empHrs=4;
+		else if(empCheck==FULL_TIME)
 			empHrs=8;
 		else
 			empHrs=0;
