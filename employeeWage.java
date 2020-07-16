58892c0f5a4cd07f020d6a2d536026927fdaee27
bhagyashree <bnsingasane@gmail.com>
2020-07-17 02:06:39 +0530

    calculating employee wage per month

--- employeeWage.java
+++ employeeWage.java
@@ -1,24 +1,37 @@
 public class employeeWage {
 
-	public static void main(String[] args) {
-		int PART_TIME=1;
-		int IS_FULL_TIME=1;
-		int EMP_WAGE_PER_HR=20;
+	public static final int PART_TIME=1;
+	public static final int FULL_TIME=2;
+	public static final int EMP_RATE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;
+	public static final int MAX_HRS_IN_MONTH=10;
+
+	public static void main(String[] args) 
+	{
 		int empHrs=0;
-		int empWage=0;
-		int empCheck=(int) Math.floor(Math.random()*10)%3;
-		switch (empCheck)
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
+		while (totalEmpHrs<MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 		{
-			case PART_TIME:
-				empHrs=4;
-				break;
-			case FULL_TIME:
-				empHrs=8;
-				break;
-			default:
-				empHrs=0;
+			totalWorkingDays++;
+			int empCheck=(int) Math.floor(Math.random()*10)%3;
+			switch (empCheck)
+			{
+				case PART_TIME:
+					empHrs=4;
+					break;
+				case FULL_TIME:
+					empHrs=8;
+					break;
+				default:
+					empHrs=0;
+			}		
+			totalEmpHrs+=empHrs;
+			System.out.println("Day:"+totalWorkingDays+"\tEmp Hr:"+empHrs);
 		}
-		empWage=empHrs*EMP_RATE_PER_HR;
-		System.out.println("Emp Wage:"+empWage);
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
+		System.out.println("Total Emp Wage:"+totalEmpWage);
 	}
+			
 }
+
