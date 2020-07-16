b7aed0b632343c107dae58067b60a801710d4279
bhagyashree <bnsingasane@gmail.com>
2020-07-17 01:11:18 +0530

    Checking Attendence of Employee

--- employeeWage.java
+++ employeeWage.java
@@ -1,1 +1,13 @@
-echo "Welcome to Employee Wage Computation Program"
+public class employeeWage {
+
+	public static void main(String[] args) {
+	//constants
+	int IS_FULL_TIME=1;
+	//Computation
+	double empCheck=Math.floor(Math.random() * 10) % 2;
+	if (empCheck == IS_FULL_TIME)
+		System.out.println("Employee is present");
+	else
+		System.out.println("Employee is absent");
+	}
+}
