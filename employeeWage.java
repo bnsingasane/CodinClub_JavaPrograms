487a1c0208a04faa060eea16d084f9ff82edc23b
bhagyashree <bnsingasane@gmail.com>
2020-07-17 01:37:35 +0530

    Calculating daily wage of employee

--- employeeWage.java
+++ employeeWage.java
@@ -4,11 +4,16 @@ echo "Welcome to Employee Wage Computation Problem"
 public class employeeWage {
 
 	public static void main(String[] args) {
-	int IS_FULL_TIME=1;
-	double empCheck=Math.floor(Math.random() * 10) % 2;
-	if (empCheck == IS_FULL_TIME)
-		System.out.println("Employee is present");
-	else
-		System.out.println("Employee is absent");
+		int IS_FULL_TIME=1;
+		int EMP_WAGE_PER_HR=20;
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs=8;
+		else
+			empHrs=0;
+		empWage=empHrs*EMP_RATE_PER_HR;
+		System.out.println("Emp Wage:"+empWage);
 	}
 }
